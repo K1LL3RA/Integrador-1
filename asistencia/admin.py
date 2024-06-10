cbee9895c080f6c9def1149bd94d4f392798fb6d
KAORI <omarusxd1234@gmail.com>
2024-06-09 20:41:53 -0500

    second commit

--- asistencia/admin.py
+++ asistencia/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Asistencia
-
+from .models import Usuario
 # Register your models here.
 admin.site.register(Asistencia)
+admin.site.register(Usuario)
